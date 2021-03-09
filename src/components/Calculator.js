424590be717376959d6b55b89a9948f1a8b268e1
Alvar Arias <AlvarArias@users.noreply.github.com>
2021-03-09 11:10:42 +0100

    Add math operations Add functions math: suma, resta, multiplica, divide

---
+++ src/components/Calculator.js
@@ -0,0 +1,17 @@
+function suma(n1, n2) {
+  return n1 + n2;
+}
+
+function resta(n1, n2) {
+  return n1 - n2;
+}
+
+function multiplica(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+export { suma, resta, multiplica, divide };
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 // Component List
 import * as pi from "./Math.js";
+import { suma, resta, multiplica, divide } from "./Calculator.js";
 
 export default function List() {
   return (
@@ -9,6 +10,12 @@ export default function List() {
         <li>{pi.default}</li>
         <li>{pi.dublePi()}</li>
         <li>{pi.triplePi()}</li>
+      </ul>
+      <ul>
+        <li>{suma(3, 2)}</li>
+        <li>{resta(3, 2)}</li>
+        <li>{multiplica(3, 2)}</li>
+        <li>{divide(3, 2)}</li>
       </ul>
     </div>
   );
