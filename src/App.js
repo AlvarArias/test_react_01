e2de349634ceda9626114f4ac70a4dab17f711b1
Alvar Arias <AlvarArias@users.noreply.github.com>
2021-03-09 09:24:56 +0100

    New component List component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import List from "./List";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <h2>Testing from React</h2>
+      <List />
+      <List />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+// Component List
+
+export default function List() {
+  return (
+    <div className="List">
+      <ul>
+        My list
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    </div>
+  );
+}
